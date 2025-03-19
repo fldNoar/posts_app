dd3f109704388f19a8d13e6c1d97b29417adafa2
fldNoar <fld.noar@gmail.com>
2025-03-19 21:13:00 +0300

    Create custom hook usePosts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PostForm from "./react/components/forms/PostForm/PostForm";
 import PostFilter from "./react/components/PostFilter";
 import CreatePostModal from "./react/popups/CreatePostModal/CreatePostModal";
 import DefaultButton from "./react/components/ui/buttons/DefaultButton";
+import {usePosts} from "./scripts/hooks/usePost";
 
 function App() {
 
@@ -18,19 +19,8 @@ function App() {
         sort: '',
         query: '',
     });
-
     const [modal, setModal] = useState(false);
-
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        }
-        return posts;
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
-    }, [filter.query, sortedPosts]);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
